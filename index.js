e860b9dd0fbf2f7fb6f8c76040a97fda2b802f64
Andrew Pang <andrewpang94@gmail.com>
2016-04-13 09:19:45 -0700

    Trying webhook in /

--- index.js
+++ index.js
@@ -12,21 +12,11 @@ app.use(bodyParser.json());
 app.get('/', function (req, res) {
 
     // res.send('Hello World! This is the bot\'s root endpoint!');
-    var messaging_events = req.body.entry[0].messaging;
-
-    for (var i = 0; i < messaging_events.length; i++) {
-
-        var event = req.body.entry[0].messaging[i];
-        var sender = event.sender.id;
-
-        if (event.message && event.message.text) {
-            var text = event.message.text;
-
-            sendTextMessage(sender, text.substring(0, 200));
-        }
+    if (req.query['hub.verify_token'] === verify_token) {
+        res.send(req.query['hub.challenge']);
     }
 
-    res.sendStatus(200);
+    res.send('Error, wrong validation token');
 
 });
 
